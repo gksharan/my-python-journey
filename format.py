0cd433477b21146c0cf6c3e50b06797dc555bcaa
gksharan <148695641+gksharan@users.noreply.github.com>
2025-08-19 22:47:07 +0530

    Create format.py

---
+++ format.py
@@ -0,0 +1,11 @@
+# Method 1
+def number_to_string(num):
+    return str(num)
+
+# Method 2
+def number_to_string(num):
+    return f"{num}"
+
+# Method 3
+def number_to_string(num):
+    return "{}".format(num)
